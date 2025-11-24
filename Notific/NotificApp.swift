7744e22088ad7dfb4f31793ab71e06aadcebda78
Vladimir Kudryashov <doctorinternet@icloud.com>
2025-11-24 18:04:17 +0100

    Update NotificApp.swift

--- Notific/NotificApp.swift
+++ Notific/NotificApp.swift
@@ -1,3 +1,11 @@
+// MARK: - INSTRUCTIONS
+/// Change Display Name for the name you want
+/// Change the icon for the icon you want
+/// Some default icons are already included in the "icons" folder
+/// Available icons: Instagram, Telegram, Gosuslugi
+
+/// THIS APPLICATION IS FOR PRANK PURPOSES ONLY.
+
 import SwiftUI
 
 @main
